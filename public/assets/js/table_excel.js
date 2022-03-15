1130fd119b9cbf0d3f4a912a63512212623ea4e6
amitkumar.cs19@bmsce.ac.in <amitkumar.cs19@bmsce.ac.in>
2022-03-15 18:37:36 +0530

    commiting

---
+++ public/assets/js/table_excel.js
@@ -0,0 +1,31 @@
+function downloadCSV(csv, filename) {
+  var csvfile;
+  var downloadLink;
+
+  csvfile = new Blob([csv], { type: "text/csv" });
+  downloadLink = document.createElement("a");
+  downloadLink.download = filename;
+  downloadLink.href = window.URL.createObjectURL(csvfile);
+  downloadLink.style.display = "none";
+
+  document.body.appendChild(downloadLink);
+  downloadLink.click();
+  console.log("insie donwload csv fun");
+}
+
+function exportTableCSV(filename) {
+  var csv = [];
+  var rows = document.querySelectorAll("table tr");
+
+  for (var i = 0; i < rows.length; i++) {
+    var row = [],
+      cols = rows[i].querySelectorAll("td,th");
+    for (var j = 0; j < cols.length; j++) {
+      row.push(cols[j].innerText);
+    }
+    csv.push(row.join(","));
+  }
+  // download csv file
+  downloadCSV(csv.join("\n"), filename);
+  console.log("insie export tablecsb");
+}
